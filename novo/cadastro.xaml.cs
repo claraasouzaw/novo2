6ab35d2847b54e8ec298e4be760ccddc87fb5c4a
Maria Clara <maria.clara@estudante.ifro.edu.br>
2024-11-25 13:43:08 -0400

    atualização tela visu

---
+++ novo/cadastro.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace novo
+{
+    /// <summary>
+    /// Lógica interna para cadastro.xaml
+    /// </summary>
+    public partial class cadastro : Window
+    {
+        public cadastro()
+        {
+            InitializeComponent();
+        }
+
+        private void Salvar_Click(object sender, RoutedEventArgs e)
+        {
+            curso curs = new curso();
+            curs.Show();
+            this.Close();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            curso c = new curso();
+            c.Show();
+        }
+    }
+}
---
+++ novo/inicio.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace novo
+{
+    /// <summary>
+    /// Interação lógica para inicio.xam
+    /// </summary>
+    public partial class inicio : Page
+    {
+        public inicio()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+        }
+    }
+}
